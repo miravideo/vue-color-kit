4eb344f8c60e63316686ce478a5d06509f60fa49
a6037220 <a6037220@163.com>
2022-07-14 16:19:45 +0800

    refactor: 支持传入固定canvas, 尺寸不需要传入

--- demo/App.vue
+++ demo/App.vue
@@ -1,31 +1,17 @@
 <template>
   <div class="page">
-    <div class="bg" :style="{ background: color }">
-      <div class="title">vue-color-kit</div>
+    <div class="bg">
       <div class="cover">
         <color-picker
           :theme="theme"
           :color="color"
           :sucker-hide="false"
           :sucker-canvas="suckerCanvas"
-          :sucker-area="suckerArea"
           @changeColor="changeColor"
-          @openSucker="openSucker"
         />
-        <img v-if="isOpenSucker" ref="cover" />
       </div>
     </div>
-    <div class="github">
-      <a href="https://github.com/anish2690/vue-color-kit" target="_blank">
-        Fork me on GitHub
-      </a>
-    </div>
-    <div
-      class="switch"
-      :class="{ 'anim-pull': inAnimation }"
-      @animationend="animationEnd"
-      @click="changeTheme"
-    />
+    <canvas id="myCanvas" style="{z-index: 999}"/>
   </div>
 </template>
 
@@ -41,64 +27,33 @@ export default {
       color: '#1BC7B1',
       suckerCanvas: null,
       suckerArea: [],
-      isOpenSucker: false,
       theme: '',
       inAnimation: false,
+      img: new Image(),
     }
+  },
+  mounted() {
+    const canvas = document.getElementById('myCanvas')
+    const ctx = canvas.getContext('2d')
+    canvas.width = 300
+    canvas.height = 200
+    canvas.top = 100;
+    canvas.left = 100;
+    this.img.src = imgCover;
+    setTimeout(() => {
+      ctx.drawImage(this.img, 0, 0, 300, 200)
+    }, 100)
+    Object.assign(canvas.style, {
+      position: 'absolute',
+      left: 0 + 'px',
+      top: 0 + 'px',
+    })
+    this.suckerCanvas = canvas;
   },
   methods: {
     changeColor(color) {
       const { r, g, b, a } = color.rgba
       this.color = `rgba(${r}, ${g}, ${b}, ${a})`
-    },
-    openSucker(isOpen) {
-      this.isOpenSucker = isOpen
-      if (isOpen) {
-        setTimeout(() => {
-          const cover = this.$refs.cover
-          cover.onload = () => {
-            if (!this.isOpenSucker) {
-              return
-            }
-            const coverRect = cover.getBoundingClientRect()
-            const canvas = this.createCanvas(cover, coverRect)
-            document.body.appendChild(canvas)
-            this.suckerCanvas = canvas
-            this.suckerArea = [
-              coverRect.left,
-              coverRect.top,
-              coverRect.left + coverRect.width,
-              coverRect.top + coverRect.height,
-            ]
-          }
-          cover.setAttribute('crossorigin', 'Anonymous')
-          cover.width = '800'
-          cover.src = imgCover
-        }, 10)
-      } else {
-        this.suckerCanvas && this.suckerCanvas.remove()
-      }
-    },
-    createCanvas(cover, coverRect) {
-      const canvas = document.createElement('canvas')
-      const ctx = canvas.getContext('2d')
-      canvas.width = coverRect.width
-      canvas.height = coverRect.height
-      ctx.drawImage(cover, 0, 0, coverRect.width, coverRect.height)
-      Object.assign(canvas.style, {
-        position: 'absolute',
-        left: coverRect.left + 'px',
-        top: coverRect.top + 'px',
-        opacity: 0,
-      })
-      return canvas
-    },
-    changeTheme() {
-      this.theme = this.theme ? '' : 'light'
-      this.inAnimation = true
-    },
-    animationEnd() {
-      this.inAnimation = false
     },
   },
 }
@@ -131,45 +86,6 @@ body {
       justify-content: space-around;
       align-items: center;
       width: 100%;
-    }
-  }
-  .github {
-    position: fixed;
-    right: 0;
-    top: 0;
-    width: 200px;
-    height: 30px;
-    line-height: 30px;
-    font-size: 14px;
-    font-weight: bold;
-    background: #a00;
-    text-align: center;
-    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.3);
-    transform: rotate(45deg) translateX(60px);
-    a {
-      display: block;
-      text-decoration: none;
-      color: #fff;
-    }
-  }
-  .switch {
-    position: fixed;
-    left: 20%;
-    top: -100px;
-    width: 2px;
-    height: 300px;
-    background: #ddd;
-    cursor: pointer;
-    &::after {
-      content: '';
-      position: absolute;
-      left: -10px;
-      bottom: -20px;
-      width: 22px;
-      height: 22px;
-      border: 2px solid #ddd;
-      border-radius: 50%;
-      box-sizing: border-box;
     }
   }
 }
--- src/color/Sucker.vue
+++ src/color/Sucker.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <svg
-      v-if="!isSucking"
       :class="{ active: isOpenSucker }"
       class="sucker"
       xmlns="http://www.w3.org/2000/svg"
@@ -12,29 +11,7 @@
         d="M13.1,8.2l5.6,5.6c0.4,0.4,0.5,1.1,0.1,1.5s-1.1,0.5-1.5,0.1c0,0-0.1,0-0.1-0.1l-1.4-1.4l-7.7,7.7C7.9,21.9,7.6,22,7.3,22H3.1C2.5,22,2,21.5,2,20.9l0,0v-4.2c0-0.3,0.1-0.6,0.3-0.8l5.8-5.8C8.5,9.7,9.2,9.6,9.7,10s0.5,1.1,0.1,1.5c0,0,0,0.1-0.1,0.1l-5.5,5.5v2.7h2.7l7.4-7.4L8.7,6.8c-0.5-0.4-0.5-1-0.1-1.5s1.1-0.5,1.5-0.1c0,0,0.1,0,0.1,0.1l1.4,1.4l3.5-3.5c1.6-1.6,4.1-1.6,5.8-0.1c1.6,1.6,1.6,4.1,0.1,5.8L20.9,9l-3.6,3.6c-0.4,0.4-1.1,0.5-1.5,0.1"
       />
     </svg>
-    <svg
-      v-if="isSucking"
-      class="sucker"
-      viewBox="-16 -16 68 68"
-      xmlns="http://www.w3.org/2000/svg"
-      stroke="#9099a4"
-    >
-      <g fill="none" fill-rule="evenodd">
-        <g transform="translate(1 1)" stroke-width="4">
-          <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
-          <path d="M36 18c0-9.94-8.06-18-18-18">
-            <animateTransform
-              attributeName="transform"
-              type="rotate"
-              from="0 18 18"
-              to="360 18 18"
-              dur="1s"
-              repeatCount="indefinite"
-            />
-          </path>
-        </g>
-      </g>
-    </svg>
+    <div id="suckerContainer"/>
   </div>
 </template>
 
@@ -44,35 +21,53 @@ import { defineComponent } from 'vue'
 export default defineComponent({
   props: {
     suckerCanvas: {
-      type: Object, // HTMLCanvasElement
+      type: HTMLCanvasElement, // HTMLCanvasElement
       default: null,
-    },
-    suckerArea: {
-      type: Array,
-      default: () => [],
     },
   },
   data() {
     return {
       isOpenSucker: false, // Whether it is in the straw state
       suckerPreview: null, // Preview color dom next to the straw
-      isSucking: false, // Is it in a straw waiting state
+      container: null,
+      top:0,
+      left:0,
+      width: 0,
+      height: 0,
+      previewSize: 110,
     }
   },
-  watch: {
-    suckerCanvas(newVal) {
-      this.isSucking = false
-      this.suckColor(newVal)
-      // newVal.style.cursor = `url('../img/sucker.png') 0 32, default`
-      //TODO
-    },
+  mounted() {
+    this.$nextTick(() => {
+      this.container = document.getElementById('suckerContainer');
+      const { top, left, width, height } = this.suckerCanvas.getBoundingClientRect();
+      this.top = top;
+      this.left = left;
+      this.width = width;
+      this.height = height;
+    });
   },
   methods: {
     openSucker() {
       if (!this.isOpenSucker) {
+        if (this.suckerCanvas && this.suckerCanvas.tagName !== 'CANVAS') {
+          return
+        }
         this.isOpenSucker = true
-        this.isSucking = true
         this.$emit('openSucker', true)
+        // @ts-ignore
+        this.suckerPreview = document.createElement('canvas');
+        this.suckerPreview.width = this.previewSize;
+        this.suckerPreview.height = this.previewSize;
+        this.suckerPreview.style.border = "1px solid #000"
+        this.suckerPreview.style.borderRadius = "50%"
+
+        this.container.appendChild(this.suckerPreview);
+
+        document.addEventListener('mousemove', this.mousemoveHandler)
+        document.addEventListener('mouseup', this.mousemoveHandler)
+
+        this.suckerPreview.addEventListener('click', this.select)
         document.addEventListener('keydown', this.keydownHandler)
       } else {
         // The processing logic is the same as pressing the esc key
@@ -83,101 +78,78 @@ export default defineComponent({
       // esc
       if (e.keyCode === 27) {
         this.isOpenSucker = false
-        this.isSucking = false
         this.$emit('openSucker', false)
         document.removeEventListener('keydown', this.keydownHandler)
         document.removeEventListener('mousemove', this.mousemoveHandler)
         document.removeEventListener('mouseup', this.mousemoveHandler)
+        this.suckerPreview.removeEventListener('click', this.select)
         if (this.suckerPreview) {
           // @ts-ignore
-          document.body.removeChild(this.suckerPreview)
+          this.container.removeChild(this.suckerPreview)
           this.suckerPreview = null
         }
       }
     },
-    mousemoveHandler(e: any) {
-      const { clientX, clientY } = e
-      const {
-        top: domTop,
-        left: domLeft,
-        width,
-        height,
-      } = this.suckerCanvas.getBoundingClientRect()
-      const x = clientX - domLeft
-      const y = clientY - domTop
+    getColor(x, y) {
       const ctx = this.suckerCanvas.getContext('2d')
       const imgData = ctx.getImageData(
-        Math.min(x, width - 1),
-        Math.min(y, height - 1),
-        1,
-        1
+          Math.min(x, this.width - 1),
+          Math.min(y, this.height - 1),
+          1,
+          1
       )
       let [r, g, b, a] = imgData.data
-      a = parseFloat((a / 255).toFixed(2))
-      // @ts-ignore
-      const style = this.suckerPreview.style
+      return {r, g, b, a}
+    },
+    mousemoveHandler(e: any) {
+      if (!this.isOpenSucker) return
+      const { clientX, clientY } = e
+
+      const x = Math.min(clientX - this.left, this.width - 1);
+      const y = Math.min(clientY - this.top, this.height - 1);
+
+      const ctx = this.suckerPreview.getContext('2d')
+      ctx.clearRect(0, 0, this.previewSize, this.previewSize);
+      const mr = 5;
+      const sx = x - mr;
+      const sy = y - mr
+      for (let _x=x - mr; _x <= x + mr;_x++) {
+        for (let _y=y - mr; _y<= y + mr ;_y++) {
+          const {r, g, b, a} = this.getColor(_x, _y)
+          ctx.fillStyle = `rgba(${r}, ${g}, ${b}, ${a})`;
+          const box = [(_x - sx) * 10, (_y - sy) * 10, 10, 10];
+          ctx.fillRect(...box);
+          if (_x === x && _y === y){
+            ctx.strokeStyle = '#000';
+            const bbox = [box[0], box[1], box[2] -1, box[3] -1]
+            ctx.strokeRect(...bbox);
+          }
+        }
+      }
+      const style = this.container.style;
       Object.assign(style, {
         position: 'absolute',
-        left: clientX + 20 + 'px',
-        top: clientY - 36 + 'px',
-        width: '24px',
-        height: '24px',
-        borderRadius: '50%',
-        border: '2px solid #fff',
-        boxShadow: '0 0 8px 0 rgba(0, 0, 0, 0.16)',
-        background: `rgba(${r}, ${g}, ${b}, ${a})`,
+        left: clientX - (this.previewSize / 2) + 'px',
+        top: clientY - (this.previewSize / 2) + 'px',
         zIndex: 95, // The level of the preview color of the small circle of the eyedropper cannot exceed the color selector
       })
-      if (
-        this.suckerArea.length &&
-        // @ts-ignore
-        clientX >= this.suckerArea[0] &&
-        // @ts-ignore
-
-        clientY >= this.suckerArea[1] &&
-        // @ts-ignore
-
-        clientX <= this.suckerArea[2] &&
-        // @ts-ignore
-
-        clientY <= this.suckerArea[3]
-      ) {
-        // @ts-ignore
-
+      if (clientX >= this.left && clientY >= this.top && clientX <= (this.left + this.width)
+          && clientY <= (this.top + this.height)) {
         style.display = ''
+        style.cursor = 'none'
       } else {
         // @ts-ignore
         style.display = 'none'
+        style.cursor = 'default'
       }
     },
-    suckColor(dom: any) {
-      if (dom && dom.tagName !== 'CANVAS') {
-        return
-      }
-      // @ts-ignore
-      this.suckerPreview = document.createElement('div')
-      // @ts-ignore
-      if (this.suckerPreview) document.body.appendChild(this.suckerPreview)
-
-      document.addEventListener('mousemove', this.mousemoveHandler)
-      document.addEventListener('mouseup', this.mousemoveHandler)
-
-      dom.addEventListener('click', (e: any) => {
+    select(e: any) {
         const { clientX, clientY } = e
-        const { top, left, width, height } = dom.getBoundingClientRect()
-        const x = clientX - left
-        const y = clientY - top
-        const ctx = dom.getContext('2d')
-        const imgData = ctx.getImageData(
-          Math.min(x, width - 1),
-          Math.min(y, height - 1),
-          1,
-          1
-        )
-        let [r, g, b, a] = imgData.data
+        const x = clientX - this.left
+        const y = clientY - this.top
+        let {r, g, b, a} = this.getColor(x, y);
         a = parseFloat((a / 255).toFixed(2))
         this.$emit('selectSucker', { r, g, b, a })
-      })
     },
   },
 })
